875d56abddc0ecfedcbdfb360d3ebc2ddfa3935c
AlienEngineer <Alien.Software.Engineer@gmail.com>
2014-04-17 15:48:30 +0100

    Scaffold: ServiceController

--- ProductiveScaffolders/CodeTemplates/Scaffolders/ServiceController/ServiceController.ps1
+++ ProductiveScaffolders/CodeTemplates/Scaffolders/ServiceController/ServiceController.ps1
@@ -1,15 +1,47 @@
 [T4Scaffolding.Scaffolder(Description = "Enter a description of ServiceController here")][CmdletBinding()]
 param(        
+	[parameter(Mandatory=$true, ValueFromPipelineByPropertyName = $true)][System.String]$type,
     [string]$Project,
 	[string]$CodeLanguage,
 	[string[]]$TemplateFolders,
-	[switch]$Force = $false
+	[switch]$Force = $false,
+	[string]$Name = "-",
+	[string]$ToFolder = "Controllers"
 )
 
-$outputPath = "ExampleOutput"  # The filename extension will be added based on the template's <#@ Output Extension="..." #> directive
 $namespace = (Get-Project $Project).Properties.Item("DefaultNamespace").Value
 
-Add-ProjectItemViaTemplate $outputPath -Template ServiceControllerTemplate `
-	-Model @{ Namespace = $namespace; ExampleValue = "Hello, world!" } `
-	-SuccessMessage "Added ServiceController output at {0}" `
-	-TemplateFolders $TemplateFolders -Project $Project -CodeLanguage $CodeLanguage -Force:$Force
+try
+{
+	$class = Get-ProjectType $type 
+
+	if ($Name -eq "-")
+	{
+		$Name = $type
+		
+		if ($class.Kind -eq 8)
+		{
+			$Name = $Name.Substring(1)
+		}
+	}
+
+	$outputPath = $outputPath = ("$ToFolder\$Name" + "Controller")
+
+	if ($class -eq $null)
+	{
+		Exit
+	}
+
+	Add-ProjectItemViaTemplate $outputPath -Template ServiceControllerTemplate `
+		-Model @{ 
+			Namespace = $namespace; 
+			Name = $Name;
+		} `
+		-SuccessMessage "Added ServiceController output at {0}" `
+		-TemplateFolders $TemplateFolders -Project $Project -CodeLanguage $CodeLanguage -Force:$Force
+
+}
+catch [System.Exception]
+{
+	Write-Error $Error[0];
+}
---
+++ ProductiveScaffolders/Controllers/ServiceController.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductiveScaffolders
+{ 
+    public class ServiceController : Controller
+    {
+		
+	}
+}
